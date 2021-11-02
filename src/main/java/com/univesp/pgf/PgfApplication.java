c9076a0033030313d526f6c48f4a5703e087f75b
Marcos Ruiz Freire <marcosruizmrf@outlook.com>
2021-11-01 23:08:26 -0300

    feat: Project and class main created

---
+++ src/main/java/com/univesp/pgf/PgfApplication.java
@@ -0,0 +1,17 @@
+package com.univesp.pgf;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+
+public class PgfApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgfApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+
+}
---
+++ src/test/java/com/univesp/pgf/PgfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univesp.pgf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
