fabffdd09ffa5ff7abc5c95609d14fdaeb3bda63
Marcos Ruiz Freire <marcosruizmrf@outlook.com>
2021-11-01 23:54:31 -0300

    feat: Resource and Service classes created

---
+++ src/main/java/com/univesp/pgf/resources/PgfResource.java
@@ -0,0 +1,79 @@
+package com.univesp.pgf.resources;
+
+public class PgfResource {
+
+}
+
+package com.univesp.pgf.resources;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.univesp.pgf.domain.Pgf;
+import com.univesp.pgf.services.PgfService;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping(value = "/pgfs")
+public class PgfResource {
+
+	@Autowired
+	private PgfService service;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Pgf> findById(@PathVariable Integer id) {
+		Pgf obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@GetMapping(value = "/open")
+	public ResponseEntity<List<Pgf>> listOpen() {
+		List<Pgf> list = service.findAllOpen();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/close")
+	public ResponseEntity<List<Pgf>> listClose() {
+		List<Pgf> list = service.findAllClose();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping
+	public ResponseEntity<List<Pgf>> listAll() {
+		List<Pgf> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@PostMapping
+	public ResponseEntity<Pgf> create(@RequestBody Pgf obj) {
+		obj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+     
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Pgf> update(@PathVariable Integer id, @RequestBody Pgf obj) {
+		Pgf newObj = service.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
+	}
+
+}
---
+++ src/main/java/com/univesp/pgf/services/PgfService.java
@@ -0,0 +1,60 @@
+package com.univesp.pgf.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sun.tools.javac.comp.Pgf;
+import com.univesp.pgf.domain.Pgf;
+import com.univesp.pgf.repositories.PgfRepository;
+
+import javassist.tools.rmi.ObjectNotFoundException;
+
+@Service
+public class PgfService {
+
+	@Autowired
+	private PgfRepository repository;
+
+	public Pgf findById(Integer id) {
+		Optional<Pgf> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pgf.class.getName()));
+	}
+
+	public List<Pgf> findAllOpen() {
+		List<Pgf> list = repository.findAllOpen();
+		return list;
+	}
+
+	public List<Pgf> findAllClose() {
+		List<Pgf> list = repository.findAllClose();
+		return list;
+	}
+
+	public List<Pgf> findAll() {
+		List<Pgf> list = repository.findAll();
+		return list;
+	}
+
+	public Pgf create(Pgf obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		repository.deleteById(id);
+	}
+
+	public Pgf update(Integer id, Pgf obj) {
+		Pgf newObj = findById(id);
+		newObj.setTitulo(obj.getTitulo());
+		newObj.setDataParaFinalizar(obj.getDataParaFinalizar());
+		newObj.setDescricao(obj.getDescricao());
+		newObj.setFinalizado(obj.getFinalizado());
+		return repository.save(newObj);
+	}
+
+}
